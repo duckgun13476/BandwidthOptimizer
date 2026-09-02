2af297348e675ca2d40eb5b03c14b726997a254c
Pink_Cats <1907284584@qq.com>
2026-09-02 17:53:20 +1000

    Support NeoForge 26.2 release runtime

--- project/neoforge/26.2/src/main/java/com/PinkCats/bandwidthoptimizer/mixin/minecraft/MinecraftDisconnect26Mixin.java
+++ project/neoforge/26.2/src/main/java/com/PinkCats/bandwidthoptimizer/mixin/minecraft/MinecraftDisconnect26Mixin.java
@@ -1,16 +1,16 @@
 package com.PinkCats.bandwidthoptimizer.mixin.minecraft;
 
 import com.PinkCats.bandwidthoptimizer.connection.ClientDisconnectHooks;
-import net.minecraft.client.gui.screens.PauseScreen;
+import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(PauseScreen.class)
-public abstract class PauseScreenDisconnect26Mixin {
+@Mixin(Minecraft.class)
+public abstract class MinecraftDisconnect26Mixin {
 
-    @Inject(method = "lambda$createPauseMenu$13()V", at = @At("HEAD"))
+    @Inject(method = "disconnectFromWorld(Lnet/minecraft/network/chat/Component;)V", at = @At("HEAD"))
     private void bandwidthoptimizer$markLocalDisconnect(CallbackInfo ci) {
         ClientDisconnectHooks.markCurrentConnectionLocal();
     }
